15e48969e7b71755412de71fbb0215e3e48e9c77
IT21199462 <99341435+IT21199462@users.noreply.github.com>
2022-05-20 23:41:45 +0530

    Create agent.h

---
+++ agent.h
@@ -0,0 +1,18 @@
+#include "User.h"
+
+
+class Agent:public User{
+	
+	private:
+		string agent_ID;
+		string password;
+		
+	public:
+		Agent(string Name,string Address,int pnumber,string email,string agentid,string pass,int pid,int disid,int percentage,Feedback *f);
+		Agent();
+		~Agent();
+		void addInsuaranceDetails();
+		void approveFeedbacks();
+	
+	
+};
