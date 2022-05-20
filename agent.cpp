073c9f8bc784a6d9f36926f5471f56282dd01301
IT21199462 <99341435+IT21199462@users.noreply.github.com>
2022-05-20 23:41:02 +0530

    Create agent.cpp

---
+++ agent.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+Agent::Agent(){
+	
+	agent_ID="";
+	password="";
+	
+}
+Agent::Agent(string Name,string Address,int pnumber,string email,string agentid,string pass,int pid,int disid,int percentage,Feedback *f)
+:User(string Name,string Address,int pnumber,string email)
+{
+	
+	agent_ID=agentid;
+	password=pass;
+	feedback[0]=f;
+	
+	
+}
+
+Agent::~Agent()
+{
+	
+	cout<<"Agent account deleting"<<endl;
+	
+}
+
+
